351bde7ab1804dd884f53928b0054a0465a57704
ommmishra <41461196+ommmishra@users.noreply.github.com>
2018-08-16 20:37:10 +0530

    Add files via upload

---
+++ ques1.java
@@ -0,0 +1,32 @@
+public class ques1 {
+ 
+	public static boolean check(int n) {
+		if(n%3 == 0 || n%5 == 0)
+		{
+			return true;
+		}
+		else
+			return false;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		int sum = 0;
+		
+		for (int i = 1; i<=1000; i++) {
+			
+			if(check(i)) 
+			{
+				
+			sum = sum + i;
+				
+			}
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
