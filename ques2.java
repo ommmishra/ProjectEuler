741c9157709dcc51da329f04be195db49b2324ee
ommmishra <41461196+ommmishra@users.noreply.github.com>
2018-08-16 23:36:30 +0530

    Add files via upload

--- ques2.java
+++ ques2.java
@@ -1,2 +1,20 @@
+public class grtr{
 
-
+	public static void main (String[]args) {
+		int f1 = 1;
+		int f2 = 1;
+		int f = 0;
+		int sum = 0;
+ 
+		while (f < 4000000) {
+			if ((f % 2) == 0) {
+				sum = sum + f;
+			}
+ 
+			f = f1 + f2;
+			f2 = f1;
+			f1 = f;
+}
+		System.out.println(sum);
+	}
+}
