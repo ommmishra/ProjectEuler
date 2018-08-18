01ff90e7b9e3f0955bb7769f9e5cae4178a1ee16
ommmishra <41461196+ommmishra@users.noreply.github.com>
2018-08-19 02:15:21 +0530

    Add files via upload

--- ques5.java
+++ ques5.java
@@ -1,42 +1,44 @@
-class grtr{
-	public static void main (String[]args) {
-		int i=2521, j=2;
-		inner:
-		while(true) {
-			outer:
-			
-				if(i%j == 0) {
-			
-				 j++;
-				if(j== 20)
-				  {
-					  System.out.println(i);
-					  break inner;
-				  } 
-				
-			    break outer;}
-			
-			
-			
-			
-			else {
-				i++;
-				j=2;
-				
-			}  
-		
-			
-			
-			   
-			}
-			
-				
-				
-			}
-			
-					
-		}
+public class grtr{
+	
+	public static long gcd(long a, long b) {
 
+	    long x;
+	    
+	    if (b > a) {
+	         x = a;
+	         a = b;
+	         b = x;
+	    }
+
+	    while (b != 0) {
+	        x = a%b;
+	        a = b;
+	        b = x ;
+
+	    }
+	  return a;
+	
+	}	
+	
+	public static long lcm(long a, long b) {
+		
+		return a*b/gcd(a, b);
+		
+	}
+	
+	
+ public static void main (String[]args) {
+	 
+	long z = 1;
+	for(int i = 2; i < 21; i++)
+		z = lcm(z,i);
+	 
+	 System.out.println(z);
+ }
+       }
+	
+			
+		
 
 	
 	
